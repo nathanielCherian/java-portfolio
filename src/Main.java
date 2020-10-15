e6d9576c5a706f85651974f8f6a65c7e605e8667
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-10-15 09:02:32 -0700

    aimboost

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import app.Home;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("HELLO WORLD");
+        Home.main(null);
+
     }
 }
 //I HATE ANTHONY SO MUCH
