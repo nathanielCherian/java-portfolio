d48a4745818b1dd1f3c94c539774a828885c33ff
tonyhieu <amv7668@gmail.com>
2020-11-04 15:51:44 -0800

    Update Bullet.java

--- src/roulette/Bullet.java
+++ src/roulette/Bullet.java
@@ -4,17 +4,11 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 public class Bullet {
-    double circleX;
-    double circleY;
-    Ellipse2D circle;
     boolean loaded;
 
 
-    public Bullet (double circleX, double circleY, boolean loaded) {
-        this.circleX = circleX;
-        this.circleY = circleY;
+    public Bullet (boolean loaded) {
         this.loaded = loaded;
-        circle = new Ellipse2D.Double(circleX, circleY, 50, 50);
     }
 
 
