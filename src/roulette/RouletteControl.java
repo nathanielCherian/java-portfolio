36730cde1bcc8b30cc2f86889cfcc14fd8faef40
tonyhieu <amv7668@gmail.com>
2020-11-06 09:50:41 -0800

    update roulette

---
+++ src/roulette/RouletteControl.java
@@ -0,0 +1,22 @@
+package roulette;
+
+public class RouletteControl {
+
+    /*public static Stack loadChamber() {
+        Stack chamber = new Stack();
+        int bullet = (int) Math.random() * (6);
+
+        for (int i = 0; i < 6; i++) {
+            if (i == bullet)
+                chamber.push(new Bullet(Bullet.STATE.REAL));
+            else
+                chamber.push(new Bullet(Bullet.STATE.BLANK));
+        }
+
+        return chamber;
+    }*/
+
+
+
+
+}
--- src/roulette/RouletteUI.java
+++ src/roulette/RouletteUI.java
@@ -1,14 +1,12 @@
 package roulette;
 
-import hangman.HangmanUI;
-
 import javax.swing.*;
 import javax.swing.border.MatteBorder;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
 
 public class RouletteUI extends JFrame {
-
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -21,22 +19,88 @@ public class RouletteUI extends JFrame {
         });
     }
 
+    //roulette.Stack chamber = new roulette.Stack();
+
+    ArrayList<Ellipse2D> c = new ArrayList<Ellipse2D>();
+
+    public void drawCircles (Graphics2D g) {
+        for (int i = 0; i < c.size(); i++)
+            c.remove(i);
+        c.add(new Ellipse2D.Double(225, 50, 50, 50));
+        c.add(new Ellipse2D.Double(150, 100, 50, 50));
+        c.add(new Ellipse2D.Double(150, 175, 50, 50));
+        c.add(new Ellipse2D.Double(300, 100, 50, 50));
+        c.add(new Ellipse2D.Double(300, 175, 50, 50));
+        c.add(new Ellipse2D.Double(225, 225, 50, 50));
+
+        g.setColor(Color.gray);
+        for (Ellipse2D e : c)
+            g.fill(e);
+    }
 
 
+    boolean dead;
+
+    /*private void fireGun() {
+        if (chamber.getObject().STATE == roulette.Bullet.STATE.REAL) {
+            dead = true;
+        } else {
+            chamber.pop();
+        }
+    }*/
+
+    int paintMode = 1;
+    int bulletNumber;
+    private void redrawBullet(Ellipse2D c, Graphics2D g) {
+
+    }
 
     public RouletteUI () {
         //setting standards for color and font
         Font f = new Font("Comic Sans MS", Font.PLAIN, 20);
         MatteBorder b = new MatteBorder(4, 4, 4, 4, Color.BLACK);
-        Color c = new Color(0, 255, 85);
+        Color r = new Color(208, 75, 75);
+        Color bl = new Color(0, 0, 0);
 
         //operations to set up the JFrame
-        getContentPane().setBackground(new Color(3, 252, 198));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setBounds(100, 100, 500, 500);
         getContentPane().setLayout(null);
 
+        //setting up JPanel
+        JPanel p = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
 
+                switch (paintMode) {
+                    case 1:
+                        drawCircles(g2d);
+                        break;
+                }
 
+            }
+        };
+        getContentPane().add(p);
+        p.setSize(500, 500);
+        p.setBackground(bl);
+
+        JButton fireButton = new JButton("Fire");
+        fireButton.setBounds(150, 350, 200, 50);
+        fireButton.setBorder(b);
+        fireButton.setBackground(r);
+        fireButton.setOpaque(true);
+        fireButton.setVisible(true);
+        getContentPane().add(fireButton);
+
+        JButton reloadButton = new JButton("Reset Chamber");
+        reloadButton.setBounds(200, 10, 100, 40);
+        reloadButton.setBorder(b);
+        reloadButton.setBackground(r);
+        reloadButton.setOpaque(true);
+        reloadButton.setVisible(true);
+        getContentPane().add(reloadButton);
     }
+
 }
